14645f7163c4debf0a0cabe34b0f5d7d01d25f09
curso.ads <curso.ads@LAB06-27>
2022-08-15 21:54:32 -0300

    Adicionar arquivos de projeto.

---
+++ aula1.1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciaçao do objeto
+            veiculo golf = new veiculo();
+            
+            Console.WriteLine("Digite a marca");
+            string marca = Console.ReadLine();
+           
+            Console.WriteLine("Digite o modelo");
+            string modelo = Console.ReadLine();
+           
+            Console.WriteLine("Digite a placa");
+            string placa = Console.ReadLine();
+           
+            Console.WriteLine("Digite o ano de fabricaçao");
+            int ano = Convert.ToInt32(Console.ReadLine());
+            
+            //objeto chamanod método atribuir
+            golf.atribuir(marca, modelo, placa, ano);
+            
+        }
+    }
+}
---
+++ aula1.1/veiculo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula1._1
+{
+    internal class veiculo
+    {
+        // Declaraçao de atributos 
+        string marca;
+        string modelo;
+        string placa;
+        int ano;
+        // método para atribuir valores 
+
+        public void atribuir
+            (string ma,
+            string mo, 
+            string pl,
+            int a)
+        {
+            this.ano = a;
+            this.marca = ma;
+            this.modelo = mo;
+            this.placa = pl;
+        }
+        public string retorna()
+        {
+            return "Marca= " + this.marca +
+                "\nModelo= " + this.modelo + 
+                "\nAno de fabricaçao= " + this.ano +
+                "\nPlaca= " + this.placa;
+        }
+    }
+}
